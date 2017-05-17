1b5f68beb0ee2e8a129117a3d3fc4013ac874e69
Benjamin Falk <benjamin.falk@cyanjoe.com>
2017-05-18 00:59:05 +0200

    Reset listener on config change

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const config = require( './lib/config' );
 const midiEvents = require( './lib/midiEvents' );
 
-midiEvents();
+let configObj = {};
+let unbindFunction = false;
+config( './config.yml', ( err, obj ) => {
+  if ( err ) {
+    return console.error( err );
+  }
+
+  if ( typeof unbindFunction === 'function' ) {
+    unbindFunction();
+  }
+
+  configObj = obj;
+  const devices = Object.keys( obj.input );
+  unbindFunction = midiEvents( devices );
+} );
