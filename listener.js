d91d99125b9545545bb7238786b90cdb24453e41
Benjamin Falk <benjamin.falk@cyanjoe.com>
2017-05-19 23:14:43 +0200

    Only initially bind when config is loaded

--- listener.js
+++ listener.js
@@ -3,11 +3,17 @@ const midi = require( './lib/midi' );
 const midiInputChangeListener = require( './lib/midiInputChangeListener' );
 const execMessage = require( './lib/execMessage' );
 
+let initialRebind = true;
 let configObj = {};
 let unbindFunction = null;
 let wantedDevices = [];
 let currentListener = null;
 function rebindListener( configObj, wantedDevices, unbindFunction ) {
+  if ( wantedDevices.length === 0 && initialRebind === true ) {
+    initialRebind = false;
+    return;
+  }
+
   if ( typeof unbindFunction === 'function' ) {
     unbindFunction();
   }
